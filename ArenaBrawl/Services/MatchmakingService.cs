26844bf16914b9247f773cce04e20c1e2f42b9a1
Andrew Jones-Weiss <andrewjonesweiss@gmail.com>
2019-11-02 22:00:48 +0000

    Initial very shoddy in-memory solution

--- ArenaBrawl/Services/MatchmakingService.cs
+++ ArenaBrawl/Services/MatchmakingService.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ArenaBrawl.Services
 {
     public class MatchmakingService
     {
+        private readonly List<Game> _games = new List<Game>();
         private readonly ConcurrentQueue<string> _queue = new ConcurrentQueue<string>();
+        private CancellationTokenSource _pollingCancellationToken;
+
+        public MatchmakingService()
+        {
+            AttemptToMatchPlayers();
+        }
 
         public Task<bool> AddToQueue(string id)
         {
@@ -15,10 +24,44 @@ namespace ArenaBrawl.Services
 
         public Task<(bool found, string matchesName)> AttemptToFindMatch(string playerId)
         {
-            var dequeueResult = _queue.TryDequeue(out var result);
-            if (result == playerId) _queue.Enqueue(result);
-
-            return Task.FromResult(dequeueResult ? (false, "") : (true, result));
+            var game = _games.Find(g => g.Players.Contains(playerId));
+            return Task.FromResult(game == null ? (false, "") : (true, game.Players.Find(p => p != playerId)));
         }
+
+        public int AmountOfPlayersInQueue()
+        {
+            return _queue.Count;
+        }
+
+        public int GamesInProgress()
+        {
+            return _games.Count;
+        }
+
+        private async void AttemptToMatchPlayers()
+        {
+            _pollingCancellationToken = new CancellationTokenSource();
+            while (!_pollingCancellationToken.IsCancellationRequested)
+            {
+                if (_queue.Count > 1)
+                {
+                    var dequeueResultOne = _queue.TryDequeue(out var resultOne);
+                    var dequeueResultTwo = _queue.TryDequeue(out var resultTwo);
+                    _games.Add(new Game(new List<string> {resultOne, resultTwo}));
+                }
+
+                await Task.Delay(5000);
+            }
+        }
+    }
+
+    public class Game
+    {
+        public Game(List<string> players)
+        {
+            Players = players;
+        }
+
+        public List<string> Players { get; }
     }
 }
