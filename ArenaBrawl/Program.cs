4a341d27ab2c41cc3acd71cd1afb63b7649e3286
Andrew Jones-Weiss <andrew.jones-weiss@spektrix.com>
2019-11-10 18:04:14 +0000

    Adds logging config for ApplicationsInsights

--- ArenaBrawl/Program.cs
+++ ArenaBrawl/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace ArenaBrawl
 {
@@ -15,6 +16,20 @@ namespace ArenaBrawl
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureLogging(builder =>
+                {
+                    // Providing an instrumentation key here is required if you're using
+                    // standalone package Microsoft.Extensions.Logging.ApplicationInsights
+                    // or if you want to capture logs from early in the application startup
+                    // pipeline from Startup.cs or Program.cs itself.
+                    builder.AddApplicationInsights("ikey");
+
+                    // Optional: Apply filters to control what logs are sent to Application Insights.
+                    // The following configures LogLevel Information or above to be sent to
+                    // Application Insights for all categories.
+                    builder.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>
+                        ("", LogLevel.Information);
                 });
     }
 }
